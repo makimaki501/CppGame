7a1dc8ebc7596051b3dec0c5322666f03b15a719
makimaki1005 <gumdam1999r@gmail.com>
2019-11-11 12:09:52 +0900

    四角形をY軸に回転

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -116,28 +116,29 @@ bool HelloWorld::init()
 		//this->addChild(sprite, 0);
 	}
 
-	a = 1;
-	b = 1;
-
+	//オープンGLのエラーコードを受ける変数
 	GLenum error;
 
 	m_pProgram = new GLProgram;
-	m_pProgram->initWithFilenames("shaders/shader_1tex.vsh", "shaders/shader_1tex.fsh");
-
+	//シェーダをテキストファイルから読み込んでコンパイル
+	m_pProgram->initWithFilenames("shaders/shader_0tex.vsh", "shaders/shader_0tex.fsh");
+	//attribute変数に属性インデックスを割り振る
 	m_pProgram->bindAttribLocation("a_position", GLProgram::VERTEX_ATTRIB_POSITION);
+	error = glGetError();
+	//attribute変数に属性インデックスを割り振る
 	m_pProgram->bindAttribLocation("a_color", GLProgram::VERTEX_ATTRIB_COLOR);
-	m_pProgram->bindAttribLocation("a_texCoord", GLProgram::VERTEX_ATTRIB_TEX_COORD);
-
+	error = glGetError();
+	//シェーダーをリンク
 	m_pProgram->link();
-
+	error = glGetError();
+	//uniform変数のリストを保存
 	m_pProgram->updateUniforms();
-
-	uniform_sampler = glGetUniformLocation(m_pProgram->getProgram(), "sampler");
-
-	m_pTexture = Director::getInstance()->getTextureCache()->addImage("detnator_logo.jpg");
+	error = glGetError();
+	//uniform変数の番号を取得
+	uniform_wvp_matrix = glGetUniformLocation(m_pProgram->getProgram(), "u_wvp_matrix");
 
 	// 背景色の指定
-	Director::getInstance()->setClearColor(Color4F(0, 1, 0, 0));
+	Director::getInstance()->setClearColor(Color4F(0, 0, 0, 0));
 
 	return true;
 }
@@ -149,63 +150,55 @@ void HelloWorld::update(float dt) {
 
 void HelloWorld::draw(Renderer* renderer, const Mat4& transform, uint32_t flags)
 {
-	GL::enableVertexAttribs(GL::VERTEX_ATTRIB_FLAG_POSITION | GL::VERTEX_ATTRIB_FLAG_COLOR | GL::VERTEX_ATTRIB_FLAG_TEX_COORD);
+	GL::enableVertexAttribs(GL::VERTEX_ATTRIB_FLAG_POSITION | GL::VERTEX_ATTRIB_FLAG_COLOR /*| GL::VERTEX_ATTRIB_FLAG_TEX_COORD*/);
 	m_pProgram->use();
 
-	Vec3 pos[6];
-	Vec4 color[6];
-	Vec2 uv[6];
-
-	counter++;
-
-	const float x = 0.7f;
-	const float y = 0.7f;
-	// 三角形１つめ
-	pos[0] = Vec3(-x - counter / 120.0f, -y + counter / 120.0f, 0);
-	pos[1] = Vec3(-x - counter / 120.0f, y + counter / 120.0f, 0);
-	pos[2] = Vec3(x - counter / 120.0f, -y + counter / 120.0f, 0);
-	// 三角形２つめ
-	pos[3] = Vec3(-x + counter / 120.0f, y - counter / 120.0f, 0);
-	pos[4] = Vec3(x + counter / 120.0f, -y - counter / 120.0f, 0);
-	pos[5] = Vec3(x + counter / 120.0f, y - counter / 120.0f, 0);
-
-	//color[0] = Vec3(0, 0, 0); // 黒
-	//color[1] = Vec3(1, 0, 0); // 赤
-	//color[2] = Vec3(0, 1, 0); // 緑
-	//color[3] = Vec3(0, 0, 1); // 青
-
-	float red = 1.0f;
-	float green = 0.0f;
-	float blue = 0.0f;
-
-	color[0] = Vec4(1, 1, 1, 1);
-	color[1] = Vec4(1, 1, 1, 1);
-	color[2] = Vec4(1, 1, 1, 1);
-	color[3] = Vec4(1, 1, 1, 1);
-	color[4] = Vec4(1, 1, 1, 1);
-	color[5] = Vec4(1, 1, 1, 1);
-
-	uv[0] = Vec2(0, 1);
-	uv[1] = Vec2(0, 0);
-	uv[2] = Vec2(1, 1);
-	uv[3] = Vec2(0, 0);
-	uv[4] = Vec2(1, 1);
-	uv[5] = Vec2(1, 0);
-	if (counter > 120) {
-		counter = -120.0f;
-	}
-
+	Vec3 pos[4];
+	Vec4 color[4];
+	const float x = 50.0f;
+	const float y = 50.0f;
+	//座標を1点ずつ設定
+	pos[0] = Vec3(-x, -y, 0);
+	pos[1] = Vec3(-x, y, 0);
+	pos[2] = Vec3(x, -y, 0);
+	pos[3] = Vec3(x, y, 0);
+	//カラーを1点ずつ設定
+	color[0] = Vec4(1, 0, 0, 1);
+	color[1] = Vec4(1, 0, 0, 1);
+	color[2] = Vec4(1, 0, 0, 1);
+	color[3] = Vec4(1, 0, 0, 1);
+	//指定した属性インデックスに、データを関連付ける
 	glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_POSITION, 3, GL_FLOAT, GL_FALSE, 0, pos);
+	//指定した属性インデックスに、データを関連付ける
 	glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_COLOR, 4, GL_FLOAT, GL_FALSE, 0, color);
-	glVertexAttribPointer(GLProgram::VERTEX_ATTRIB_TEX_COORD, 2, GL_FLOAT, GL_FALSE, 0, uv);
 
-	glUniform1i(uniform_sampler, 0);
-	GL::bindTexture2D(m_pTexture->getName());
+	//ワールドビュープロジェクション行列の作成
+	static float yaw = 0.0f;
+	yaw += 0.01f;
+	Mat4 matProjection;
+	Mat4 matView;
+	Mat4 matWVP;
+	Mat4 matTrans, matSclae, matRot, matWorld;
+	//プロジェクション行列(射影行列)を取得
+	matProjection = _director->getMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_PROJECTION);
+	//ビュー行列を取得
+	matView = _director->getMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW);
+
+	//平行移動行列を作成
+	Mat4::createTranslation(Vec3(250, 50, 0), &matTrans);
+	//回転行列を作成
+	Mat4::createRotationY(yaw, &matRot);
+	//スケーリング行列を作成
+	Mat4::createScale(Vec3(1, 1, 1), &matSclae);
+	//ワールド行列を合成
+	matWorld = matTrans * matRot * matSclae;
+	//WVP行列を合成
+	matWVP = matProjection * matView * matWorld;
+	//合成したWVP行列をシェーダに送る
+	glUniformMatrix4fv(uniform_wvp_matrix, 1, GL_FALSE, matWVP.m);
 
 	//            図形         先頭番号　頂点数
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-
-
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 }
 
 
